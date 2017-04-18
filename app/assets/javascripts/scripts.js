b0d57bf868c4ee58cadf0d36cf2551bc334ec6f7
kortirso <kortirso@gmail.com>
2017-04-18 21:40:33 +0300

    fixes

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,28 +1,10 @@
 function get_tasks() {
-    //$('table tbody tr').each(function() {
-        //$(this).remove();
-    //});
-
-    $.get('http://localhost:3000/api/v1/tasks', function(data) {
+    $.get('http://localhost:5000/api/v1/tasks', function(data) {
         $(data).each(function() {
             $('table tbody').append('<tr class="task" id="task_' + this.id + '"><td>' + this.short_filename + '</td><td>' + this.from + '</td><td>' + this.to + '</td><td class="' + this.status + '">' + this.status + '</td><td><a download="' + this.result_short_filename + '" href="/uploads/task/result_file/' + this.id + '/' + this.result_short_filename + '">Download</a></td></tr>');
         });
     });
-
-    //if(all_tasks != done_tasks) {
-        //sleep(5000);
-        //get_tasks();
-    //}
 }
-
-//function sleep(milliseconds) {
-//    var start = new Date().getTime();
-//    for (var i = 0; i < 1e7; i++) {
-//        if ((new Date().getTime() - start) > milliseconds){
-//            break;
-//        }
-//    }
-//}
 
 $(function() {
     get_tasks();
