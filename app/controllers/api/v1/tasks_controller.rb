a32ae3916c11746790237bb930096399bb653d50
kortirso <kortirso@gmail.com>
2017-04-28 17:21:57 +0300

    remove api endpoint tasks#show

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,24 +1,14 @@
 class Api::V1::TasksController < Api::V1::BaseController
     before_action :authenticate_token
     before_action :select_tasks, only: :index
-    before_action :find_task, only: :show
 
     def index
         render json: { tasks: ActiveModel::Serializer::CollectionSerializer.new(@tasks, each_serializer: TaskSerializer) }
-    end
-
-    def show
-        render json: { task: TaskSerializer.new(@task) }
     end
 
     private
 
     def select_tasks
         @tasks = @user.present? ? @user.tasks.order(id: :desc) : Task.for_guest(params[:access_token]).order(id: :desc)
-    end
-
-    def find_task
-        @task = Task.find_by(id: params[:id])
-        render json: { error: 'Task does not exist' } if @task.nil?
     end
 end
