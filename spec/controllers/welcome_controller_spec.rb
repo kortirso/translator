355fb84b87b4bc0e440320a8972245e2f114775c
kortirso <kortirso@gmail.com>
2017-05-04 06:02:49 +0300

    add welcome_controller_spec

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,3 +1,9 @@
 RSpec.describe WelcomeController, type: :controller do
-    
+    describe 'GET #index' do
+        it 'should render welcome#index' do
+            get :index, params: { locale: 'en' }
+
+            expect(response).to render_template :index
+        end
+    end
 end
