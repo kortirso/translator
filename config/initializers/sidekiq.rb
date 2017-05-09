7803f51346f49abd95f30e063ad941073e6f294b
kortirso <kortirso@gmail.com>
2017-05-09 06:08:40 +0300

    fix sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,9 @@
 require 'sidekiq'
-require 'sidekiq-status'
 
 Sidekiq.configure_client do |config|
-    config.client_middleware do |chain|
-        chain.add Sidekiq::Status::ClientMiddleware
-    end
     config.redis = { namespace: 'translator', url: 'redis://127.0.0.1:6379/1' }
 end
 
 Sidekiq.configure_server do |config|
-    config.server_middleware do |chain|
-        chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes
-    end
     config.redis = { namespace: 'translator', url: 'redis://127.0.0.1:6379/1' }
 end
