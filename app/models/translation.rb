ed1454bf5308561eae44c5104116f0a88eb134fb
kortirso <kortirso@gmail.com>
2017-05-14 22:29:46 +0300

    render posible translations

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -3,6 +3,7 @@ class Translation < ApplicationRecord
     belongs_to :result, class_name: 'Word'
 
     has_many :positions, dependent: :destroy
+    has_many :tasks, through: :positions
 
     validates :base_id, :result_id, presence: true
 end
