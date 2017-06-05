b13c2fdc6ae6cdbd392b79627412cebccf725412
kortirso <kortirso@gmail.com>
2017-06-05 12:49:20 +0300

    add selecting locale in translations box component

--- app/javascript/components/translations_box/translations_box.jsx
+++ app/javascript/components/translations_box/translations_box.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TranslationsLocale from 'components/translations_box/translations_locale';
 import LocalizedStrings from 'react-localization';
 import I18nData from './i18n_data.json';
 
@@ -9,7 +10,7 @@ class TranslationsBox extends React.Component {
     constructor() {
         super();
         this.state = {
-
+            locale: null
         }
     }
 
@@ -17,14 +18,14 @@ class TranslationsBox extends React.Component {
         strings.setLanguage(this.props.locale);
     }
 
-    componentDidMount() {
-
+    _selectOption(value) {
+        this.setState({locale: value})
     }
 
     render() {
         return (
             <div>
-
+                <TranslationsLocale locale={this.props.locale} onSelectOption={this._selectOption.bind(this)} />
             </div>
         );
     }
