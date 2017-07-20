2c60d57f47ae672449a929e040afc427db2268d8
kortirso <kortirso@gmail.com>
2017-07-20 21:41:48 +0300

    add spec for users#me, add shared API Auth for unexisted users

--- spec/api/v1/users_controller_spec.rb
+++ spec/api/v1/users_controller_spec.rb
@@ -1,7 +1,7 @@
 describe 'Users API' do
     describe 'POST #create' do
         context 'with valid data' do
-            let(:request) { post '/api/v1/users', params: { user: { email: 'some@email.com', username: 'username', password: 'password12', password_confirmation: 'password12' } } }
+            let(:request) { post '/api/v1/users', params: { user: { email: 'some@email.com', username: 'username', password: 'password12', password_confirmation: 'password12' }, format: :json } }
 
             it 'creates new user record' do
                 expect { request }.to change(User, :count).by(1)
@@ -12,6 +12,10 @@ describe 'Users API' do
 
                 it 'returns 201 status' do
                     expect(response.code).to eq '201'
+                end
+
+                it 'returns serialized user object' do
+                    expect(response.body).to be_json_eql(UserSerializer.new(User.last).serializable_hash.to_json).at_path('user')
                 end
 
                 %w[id email username access_token].each do |attr|
@@ -34,7 +38,7 @@ describe 'Users API' do
 
         context 'with invalid data' do
             let!(:user) { create :user }
-            let(:request) { post '/api/v1/users', params: { user: { email: user.email, username: user.username, password: 'password12', password_confirmation: 'password12' } } }
+            let(:request) { post '/api/v1/users', params: { user: { email: user.email, username: user.username, password: 'password12', password_confirmation: 'password12' }, format: :json } }
 
             it 'doesnt create new user record' do
                 expect { request }.to_not change(User, :count)
@@ -58,13 +62,19 @@ describe 'Users API' do
         let!(:user) { create :user }
 
         context 'with valid data' do
-            let(:request) { get '/api/v1/users/access_token', params: { email: user.email, password: user.password } }
+            let(:request) { get '/api/v1/users/access_token', params: { email: user.email, password: user.password, format: :json } }
 
             context 'in answer' do
                 before { request }
 
                 it 'returns 200 status' do
                     expect(response.code).to eq '200'
+                end
+
+                it 'returns serialized user object' do
+                    user.reload
+
+                    expect(response.body).to be_json_eql(UserSerializer.new(user).serializable_hash.to_json).at_path('user')
                 end
 
                 it 'access_token is not empty' do
@@ -74,7 +84,7 @@ describe 'Users API' do
         end
 
         context 'with invalid data' do
-            let(:request) { get '/api/v1/users/access_token', params: { email: 'wrong@gmail.com', password: user.password } }
+            let(:request) { get '/api/v1/users/access_token', params: { email: 'wrong@gmail.com', password: user.password, format: :json } }
 
             context 'in answer' do
                 before { request }
@@ -87,6 +97,27 @@ describe 'Users API' do
                     expect(JSON.parse(response.body)).to eq('error' => 'Unauthorized')
                 end
             end
+        end
+    end
+
+    describe 'GET #me' do
+        it_behaves_like 'API Auth'
+
+        context 'for existed users' do
+            let!(:user) { create :user }
+            before { get '/api/v1/users/me', params: { email: user.email, access_token: user.access_token, format: :json } }
+
+            it 'returns 200 status' do
+                expect(response.code).to eq '200'
+            end
+
+            it 'returns serialized user object' do
+                expect(response.body).to be_json_eql(UserSerializer.new(user).serializable_hash.to_json).at_path('user')
+            end
+        end
+
+        def do_request(options = {})
+            get '/api/v1/users/me', params: { format: :json }.merge(options)
         end
     end
 end
