075a2a9cf9f0896e8fff185800ba54ffa1a51324
kortirso <kortirso@gmail.com>
2017-07-22 16:10:48 +0300

    fix task adding

--- app/javascript/components/tasks_box/task.jsx
+++ app/javascript/components/tasks_box/task.jsx
@@ -5,40 +5,36 @@ class Task extends React.Component {
     constructor() {
         super();
         this.state = {
-            task: {}
+
         }
     }
 
-    componentWillMount() {
-        this.setState({task: this.props.task});
-    }
-
     _checkStatus() {
-        if (this.state.task.status == 'failed') return <span>{this.props.strings.error}: {this.state.task.error_message}</span>;
-        else return this.state.task.status;
+        if (this.props.task.status == 'failed') return <span>{this.props.strings.error}: {this.props.task.error_message}</span>;
+        else return this.props.task.status;
     }
 
     _checkDownloading() {
-        if (this.state.task.status == 'done') return <a download={this.state.task.result_short_filename} href={'/uploads/task/result_file/' + this.state.task.id + '/' + this.state.task.result_short_filename}>{this.props.strings.download}</a>;
+        if (this.props.task.status == 'done') return <a download={this.props.task.result_short_filename} href={'/uploads/task/result_file/' + this.props.task.id + '/' + this.props.task.result_short_filename}>{this.props.strings.download}</a>;
     }
 
     _checkTranslation() {
         let locale = '';
         if (this.props.strings.language != 'en') locale = '/' + this.props.strings.language;
-        if (this.state.task.status == 'done') return <a href={locale + '/tasks/' + this.state.task.id}>{this.props.strings.goto}</a>;
+        if (this.props.task.status == 'done') return <a href={locale + '/tasks/' + this.props.task.id}>{this.props.strings.goto}</a>;
     }
 
     _handleDelete(event) {
         event.preventDefault;
-        this.props.onDelete(this.state.task);
+        this.props.onDelete(this.props.task);
     }
 
     render() {
         return (
-            <tr className='task' id={'task_' + this.state.task.id}>
-                <td>{this.state.task.short_filename}</td>
-                <td>{this.state.task.to}</td>
-                <td className={this.state.task.status}>{this._checkStatus()}</td>
+            <tr className='task' id={'task_' + this.props.task.id}>
+                <td>{this.props.task.short_filename}</td>
+                <td>{this.props.task.to}</td>
+                <td className={this.props.task.status}>{this._checkStatus()}</td>
                 <td>{this._checkDownloading()}</td>
                 <td>{this._checkTranslation()}</td>
                 <td><a onClick={this._handleDelete.bind(this)}>X</a></td>
