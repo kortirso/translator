2744e22e9b27ac3808401320fadbff52f8a58212
kortirso <kortirso@gmail.com>
2017-07-23 21:10:02 +0300

    update webpack

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,41 +14,13 @@ set :linked_files, fetch(:linked_files, []).push('config/application.yml')
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+#Rake::Task['deploy:assets:precompile'].clear_actions
+
 namespace :deploy do
     desc 'Restart application'
     task :restart do
         on roles(:app), in: :sequence, wait: 5 do
             execute :touch, release_path.join('tmp/restart.txt')
-        end
-    end
-
-    namespace :assets do
-        desc "Precompile assets"
-        task :precompile do
-            on roles(:app) do
-                invoke 'deploy:assets:precompile_locally_archive'
-                #Rake::Task["deploy:assets:precompile_locally_archive"].invoke
-            end
-        end    
-    end
-
-    namespace :assets do
-        desc 'Precompile assets locally and upload to server'
-        task :precompile_locally_copy do
-            on roles(:app) do
-                run_locally do
-                    with rails_env: fetch(:rails_env) do
-                        #execute 'rake assets:precompile'
-                    end
-                end
-
-                execute "cd #{release_path} && mkdir public" rescue nil
-                execute "cd #{release_path} && mkdir public/assets" rescue nil
-                execute 'rm -rf public/assets/*'
-
-                upload! 'public/assets', "#{release_path}/public", recursive: true
-
-            end
         end
     end
 
