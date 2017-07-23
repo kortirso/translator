773f7963c606edaed9e682e4141299fd80bca909
kortirso <kortirso@gmail.com>
2017-07-23 21:15:02 +0300

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,8 +14,6 @@ set :linked_files, fetch(:linked_files, []).push('config/application.yml')
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-#Rake::Task['deploy:assets:precompile'].clear_actions
-
 namespace :deploy do
     desc 'Restart application'
     task :restart do
