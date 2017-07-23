d476a8ba47ee58a491cfc89bbaf587c9199ef876
kortirso <kortirso@gmail.com>
2017-07-23 19:55:23 +0300

    add local assets:precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,5 +22,35 @@ namespace :deploy do
         end
     end
 
+    namespace :assets do
+        desc "Precompile assets"
+        task :precompile do
+            on roles(:app) do
+                invoke 'deploy:assets:precompile_locally_archive'
+                #Rake::Task["deploy:assets:precompile_locally_archive"].invoke
+            end
+        end    
+    end
+
+    namespace :assets do
+        desc 'Precompile assets locally and upload to server'
+        task :precompile_locally_copy do
+            on roles(:app) do
+                run_locally do
+                    with rails_env: fetch(:rails_env) do
+                        #execute 'rake assets:precompile'
+                    end
+                end
+
+                execute "cd #{release_path} && mkdir public" rescue nil
+                execute "cd #{release_path} && mkdir public/assets" rescue nil
+                execute 'rm -rf public/assets/*'
+
+                upload! 'public/assets', "#{release_path}/public", recursive: true
+
+            end
+        end
+    end
+
     after :publishing, :restart
 end
