0f312cf21e3462dadb440d7dd07ce459c6b879ea
kortirso <kortirso@gmail.com>
2017-08-09 16:48:39 +0300

    remove variables translation for yaml-style, add specs

--- app/services/checks/sentence_service.rb
+++ app/services/checks/sentence_service.rb
@@ -3,16 +3,38 @@ module Checks
     class SentenceService
         def self.call(value)
             @blocks = []
-            rebuilded_sentence = value.split('.').collect { |sentence| checks(sentence) }.join('.')
+            sentences = sentence_splitter(value)
+            rebuilded_sentence = sentences.collect { |sentence| checks(sentence) }.join
             {
                 sentence: rebuilded_sentence,
                 blocks_for_translate: @blocks
             }
         end
 
+        def self.sentence_splitter(value)
+            sentences = value.split('.')
+            sentences[-1] += '.' if value[-1] == '.'
+            (0..(sentences.size - 2)).each.with_index do |index|
+                sentences[index] += '.'
+            end
+            sentences
+        end
+
         def self.checks(sentence)
-            @blocks.push sentence
-            "_###{sentence}##_"
+            splitted_sentence = sentence.split(/[%][{](\w*)[}]/)
+            variables = sentence.scan(/[%][{](\w*)[}]/)
+
+            splitted_sentence.map! do |elem|
+                if variables.include?([elem])
+                    "%{#{elem}}"
+                elsif elem == ''
+                    elem
+                else
+                    @blocks.push elem
+                    "_###{elem}##_"
+                end
+            end
+            splitted_sentence.join
         end
     end
 end
---
+++ spec/services/checks/sentence_service_spec.rb
@@ -0,0 +1,111 @@
+RSpec.describe Checks::SentenceService, type: :service do
+    describe '.call' do
+        context 'for simple sentence without dot' do
+            let(:sentence) { 'Page not found' }
+
+            it 'returns modified sentence and array for translate' do
+                answer = Checks::SentenceService.call(sentence)
+
+                expect(answer[:sentence]).to eq "_###{sentence}##_"
+                expect(answer[:blocks_for_translate]).to eq [sentence]
+            end
+        end
+
+        context 'for simple sentence with dot' do
+            let(:sentence) { 'Page not found.' }
+
+            it 'returns modified sentence and array for translate' do
+                answer = Checks::SentenceService.call(sentence)
+
+                expect(answer[:sentence]).to eq "_###{sentence}##_"
+                expect(answer[:blocks_for_translate]).to eq [sentence]
+            end
+        end
+
+        context 'for 2 sentences with 1 dot' do
+            let(:sentence) { 'Page not found. 1' }
+
+            it 'returns modified sentence and array for translate' do
+                answer = Checks::SentenceService.call(sentence)
+
+                expect(answer[:sentence]).to eq '_##Page not found.##__## 1##_'
+                expect(answer[:blocks_for_translate]).to eq ['Page not found.', ' 1']
+            end
+        end
+
+        context 'for 2 sentences with 2 dots' do
+            let(:sentence) { 'Page not found. 1.' }
+
+            it 'returns modified sentence and array for translate' do
+                answer = Checks::SentenceService.call(sentence)
+
+                expect(answer[:sentence]).to eq '_##Page not found.##__## 1.##_'
+                expect(answer[:blocks_for_translate]).to eq ['Page not found.', ' 1.']
+            end
+        end
+
+        context 'for sentence with 1 variable' do
+            let(:sentence) { 'Page not found, %{name}' }
+
+            it 'returns modified sentence and array for translate with untranslated variables' do
+                answer = Checks::SentenceService.call(sentence)
+
+                expect(answer[:sentence]).to eq '_##Page not found, ##_%{name}'
+                expect(answer[:blocks_for_translate]).to eq ['Page not found, ']
+            end
+        end
+
+        context 'for sentence with 2 variables' do
+            let(:sentence) { '%{name_1}, Page not found, %{name_2}' }
+
+            it 'returns modified sentence and array for translate with untranslated variables' do
+                answer = Checks::SentenceService.call(sentence)
+
+                expect(answer[:sentence]).to eq '%{name_1}_##, Page not found, ##_%{name_2}'
+                expect(answer[:blocks_for_translate]).to eq [', Page not found, ']
+            end
+        end
+    end
+
+    describe '.sentence_splitter' do
+        context 'for simple sentence without dot' do
+            let(:sentence) { 'Page not found' }
+
+            it 'returns sentence in array without dot' do
+                answer = Checks::SentenceService.sentence_splitter(sentence)
+
+                expect(answer).to eq [sentence]
+            end
+        end
+
+        context 'for simple sentence with dot' do
+            let(:sentence) { 'Page not found.' }
+
+            it 'returns sentence in array with dot' do
+                answer = Checks::SentenceService.sentence_splitter(sentence)
+
+                expect(answer).to eq [sentence]
+            end
+        end
+
+        context 'for 2 sentences with 1 dot' do
+            let(:sentence) { 'Page not found. 1' }
+
+            it 'returns sentences in array with 1 dot' do
+                answer = Checks::SentenceService.sentence_splitter(sentence)
+
+                expect(answer).to eq ['Page not found.', ' 1']
+            end
+        end
+
+        context 'for 2 sentences with 2 dots' do
+            let(:sentence) { 'Page not found. 1.' }
+
+            it 'returns sentences in array with 2 dots' do
+                answer = Checks::SentenceService.sentence_splitter(sentence)
+
+                expect(answer).to eq ['Page not found.', ' 1.']
+            end
+        end
+    end
+end
