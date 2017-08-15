f6c716f677bce475b5e9ed90ed3207b29553c2fc
kortirso <kortirso@gmail.com>
2017-08-15 17:30:21 +0300

    fix for untranslated arrays in yml

--- app/services/checks/sentence_service.rb
+++ app/services/checks/sentence_service.rb
@@ -2,13 +2,17 @@ module Checks
     # Split sentences, remove untranslated things
     class SentenceService
         def self.call(value, extension, blocks = [], rebuilded_sentence = [])
-            sentence_splitter(value).each do |sentence|
-                check = checks(sentence, extension)
-                rebuilded_sentence.push check[:sentence]
-                blocks += check[:blocks_for_translate]
+            if value.is_a?(String)
+                sentence_splitter(value).each do |sentence|
+                    check = checks(sentence, extension)
+                    rebuilded_sentence.push check[:sentence]
+                    blocks += check[:blocks_for_translate]
+                end
+                rebuilded_sentence = rebuilded_sentence.join('.')
+                rebuilded_sentence[-1] += '.' if value[-1] == '.'
+            else
+                rebuilded_sentence = value
             end
-            rebuilded_sentence = rebuilded_sentence.join('.')
-            rebuilded_sentence[-1] += '.' if value[-1] == '.'
             {
                 sentence: rebuilded_sentence,
                 blocks_for_translate: blocks
