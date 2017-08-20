ea0286143dbbbbebd29071c140aebe12260f347c
kortirso <kortirso@gmail.com>
2017-08-20 10:05:27 +0300

    add additional untranslated text for android xml

--- app/services/fileresponders/xml.rb
+++ app/services/fileresponders/xml.rb
@@ -4,7 +4,7 @@ module Fileresponders
         include Fileresponders::Base
 
         def check_permissions
-            for_translate = base_data.xpath('//resources/string')
+            for_translate = base_data.xpath('//resources/string') + base_data.xpath('//resources/string-array/item')
             return task.failure(301) if task.user.nil? && for_translate.size > GUEST_LIMIT
             return task.failure(302) if task.user.present? && for_translate.size > USER_LIMIT
             true
@@ -13,7 +13,8 @@ module Fileresponders
         private
 
         def strings_for_translate
-            base_data.xpath('//resources/string').each do |value|
+            for_translate = (base_data.xpath('//resources/string') + base_data.xpath('//resources/string-array/item')).select { |tag| tag.attributes['translatable'].nil? || tag.attributes['translatable'].value != 'false' }
+            for_translate.each do |value|
                 checked = sentence_service.call(value.children.to_s)
                 words_for_translate.push checked[:blocks_for_translate]
                 value.children = checked[:sentence]
