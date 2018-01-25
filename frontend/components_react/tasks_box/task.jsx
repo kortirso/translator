23121b498eac6117e5a913a9b2fd3e80ee66d862
kortirso <kortirso@gmail.com>
2018-01-25 23:20:01 +0300

    redesign of main page

--- frontend/components_react/tasks_box/task.jsx
+++ frontend/components_react/tasks_box/task.jsx
@@ -2,38 +2,38 @@ import React from 'react';
 
 class Task extends React.Component {
 
-    _checkStatus() {
-        if (this.props.task.status == 'failed') return <span>{this.props.strings.error}: {this.props.task.error_message}</span>;
-        else return this.props.task.status;
-    }
+  _checkStatus() {
+    if (this.props.task.status == 'failed') return <span>{this.props.strings.error}: {this.props.task.error_message}</span>;
+    else return this.props.task.status;
+  }
 
-    _checkDownloading() {
-        if (this.props.task.status == 'done') return <a download={this.props.task.result_short_filename} href={'/uploads/task/result_file/' + this.props.task.id + '/' + this.props.task.result_short_filename}>{this.props.strings.download}</a>;
-    }
+  _checkDownloading() {
+    if (this.props.task.status == 'done') return <a download={this.props.task.result_short_filename} href={'/uploads/task/result_file/' + this.props.task.id + '/' + this.props.task.result_short_filename}>{this.props.strings.download}</a>;
+  }
 
-    _checkTranslation() {
-        let locale = '';
-        if (this.props.strings.language != 'en') locale = '/' + this.props.strings.language;
-        if (this.props.task.status == 'done') return <a href={locale + '/tasks/' + this.props.task.id}>{this.props.strings.goto}</a>;
-    }
+  _checkTranslation() {
+    let locale = '';
+    if (this.props.strings.language != 'en') locale = '/' + this.props.strings.language;
+    if (this.props.task.status == 'done') return <a href={locale + '/tasks/' + this.props.task.id}>{this.props.strings.goto}</a>;
+  }
 
-    _handleDelete(event) {
-        event.preventDefault;
-        this.props.onDelete(this.props.task);
-    }
+  _handleDelete(event) {
+    event.preventDefault;
+    this.props.onDelete(this.props.task);
+  }
 
-    render() {
-        return (
-            <tr className='task' id={'task_' + this.props.task.id}>
-                <td>{this.props.task.short_filename}</td>
-                <td>{this.props.task.to}</td>
-                <td className={this.props.task.status}>{this._checkStatus()}</td>
-                <td>{this._checkDownloading()}</td>
-                <td>{this._checkTranslation()}</td>
-                <td><a onClick={this._handleDelete.bind(this)}>X</a></td>
-            </tr>
-        );
-    }
+  render() {
+    return (
+      <tr className='task' id={'task_' + this.props.task.id}>
+        <td>{this.props.task.short_filename}</td>
+        <td>{this.props.task.to}</td>
+        <td className={this.props.task.status}>{this._checkStatus()}</td>
+        <td>{this._checkDownloading()}</td>
+        <td>{this._checkTranslation()}</td>
+        <td><a onClick={this._handleDelete.bind(this)}>X</a></td>
+      </tr>
+    );
+  }
 }
 
 export default Task;
