7517a64a2a1c2dca6313edbdf463a30a29d51176
kortirso <kortirso@gmail.com>
2018-04-02 11:28:31 +0700

    add specs for welcome and tasks #index actions

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,9 +1,23 @@
 RSpec.describe TasksController, type: :controller do
   describe 'GET #index' do
-    before { get :index, params: { locale: 'en' } }
+    context 'for html request' do
+      before { get :index, params: { locale: 'en' } }
 
-    it 'should render tasks#index' do
-      expect(response).to render_template :index
+      it 'should render tasks#index' do
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'for json request' do
+      before { get :index, params: { locale: 'en', format: :json } }
+
+      it 'should render json' do
+        resp = JSON.parse(response.body)
+
+        expect(resp['tasks']).to_not eq nil
+        expect(resp['locales']).to_not eq nil
+        expect(resp['frameworks']).to_not eq nil
+      end
     end
   end
 
