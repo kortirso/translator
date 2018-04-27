71a021027171dbb51f59c4ca76e92c845cbcbf69
kortirso <kortirso@gmail.com>
2018-04-27 16:06:50 +0700

    change guest belonging

--- app/controllers/concerns/cookies_controller.rb
+++ app/controllers/concerns/cookies_controller.rb
@@ -3,11 +3,15 @@ module CookiesController
   include CookiesHelper
 
   included do
+    after_action :attach_guest, only: :create
     after_action :remember_user, only: :create
   end
 
-  private def remember_user
+  private def attach_guest
     current_person_in_cookies.attach_to_user(current_user) if current_user.present? && current_person_in_cookies.is_a?(Guest)
+  end
+
+  private def remember_user
     remember(current_user) if current_user.present? && params[:remember_me] == '1' || params[:remember] == '1'
   end
 end
