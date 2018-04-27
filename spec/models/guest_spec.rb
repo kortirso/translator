3d86b2c7145a4520d4e64172ca88ffa000c821be
kortirso <kortirso@gmail.com>
2018-04-27 18:59:37 +0700

    add guest model specs

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe Guest, type: :model do
 
     expect(guest).to be_valid
   end
+
+  describe 'methods' do
+    context '.attach_to_user' do
+      let!(:guest) { create :guest }
+      let!(:user) { create :user }
+      let!(:task) { create :task, personable: guest }
+
+      it 'updates guest for belonging to user' do
+        expect { guest.attach_to_user(user) }.to change(guest, :user_id).to(user.id)
+      end
+
+      it 'updates guest tasks for belonging to user' do
+        guest.attach_to_user(user)
+        task.reload
+
+        expect(task.personable).to eq user
+      end
+    end
+  end
 end
