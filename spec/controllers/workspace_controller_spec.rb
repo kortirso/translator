8c40d9c75ceb2c814cd8579045308e8dc82248e2
kortirso <kortirso@gmail.com>
2018-05-12 15:21:14 +0700

    add specs for workspace controller

---
+++ spec/controllers/workspace_controller_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe WorkspaceController, type: :controller do
+  describe 'GET #index' do
+    it_behaves_like 'Workspace Auth'
+
+    context 'logged user' do
+      sign_in_user
+
+      it 'renders workspace#index' do
+        get :index, params: { locale: 'en' }
+
+        expect(response).to render_template :index
+      end
+    end
+
+    def do_request
+      get :index, params: { locale: 'en' }
+    end
+  end
+end
