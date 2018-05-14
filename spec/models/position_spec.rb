0c93104360f38ebaa50b451c4e62f95fd1cc5747
kortirso <kortirso@gmail.com>
2018-05-14 13:12:19 +0700

    add specs for position model

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe Position, type: :model do
 
     expect(position).to be_valid
   end
+
+  describe 'methods' do
+    context '.update_phrases_value' do
+      let!(:position) { create :position }
+      let!(:word) { create :word, :en }
+      let!(:phrase_1) { create :phrase, position: position, current_value: '1', word: word }
+      let!(:phrase_2) { create :phrase, position: position, current_value: '2', word: word }
+      before { position.update(temp_value: "_###{phrase_1.id}##__###{phrase_2.id}##_") }
+
+      it 'updates phrases_value of position with values from phrases' do
+        expect { position.update_phrases_value }.to change(position, :phrases_value)
+      end
+
+      it 'does not update temp_value of position' do
+        expect { position.update_phrases_value }.to_not change(position, :temp_value)
+      end
+    end
+  end
 end
