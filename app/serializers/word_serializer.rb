8444ed202db3a187f097d72dbecb53a756e07611
kortirso <kortirso@gmail.com>
2018-05-14 13:00:49 +0700

    fix specs

--- app/serializers/word_serializer.rb
+++ app/serializers/word_serializer.rb
@@ -1,6 +1,6 @@
 class WordSerializer < ActiveModel::Serializer
   attributes :id, :text, :translations
-  
+
   def translations
     object.select_translations(locale: Locale.find_by(code: @instance_options[:locale])).keys
   end
--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -3,8 +3,6 @@ RSpec.describe Position, type: :model do
   it { should have_many(:phrases).dependent(:destroy) }
   it { should validate_presence_of :task }
   it { should validate_presence_of :base_value }
-  it { should validate_presence_of :temp_value }
-  it { should validate_presence_of :phrases_value }
 
   it 'should be valid' do
     position = create :position
